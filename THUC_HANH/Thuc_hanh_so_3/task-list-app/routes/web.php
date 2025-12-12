d1863812879884aebd2f09b85a17ad980311a12a
Lý Đình Sơn <2351170615@e.tlu.edu.vn>
2025-12-12 14:24:46 +0700

    thêm route cho posts

--- THUC_HANH/Thuc_hanh_so_3/task-list-app/routes/web.php
+++ THUC_HANH/Thuc_hanh_so_3/task-list-app/routes/web.php
@@ -1,9 +1,14 @@
 <?php
 
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\PostsController;
 use App\Http\Controllers\TasksController;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', [TasksController::class, 'index']);
 
 Route::resource('/tasks', TasksController::class);
+
+
+Route::get('posts', [PostsController::class, 'index']);
