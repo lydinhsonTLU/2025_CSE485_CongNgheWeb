e86063cbaecdd6ea71b8412b48f7aafaf0381de0
Lý Đình Sơn <2351170615@e.tlu.edu.vn>
2025-12-20 11:32:09 +0700

    no commit change

--- PHT/PHT_6/resources/views/listSinhvien.blade.php
+++ PHT/PHT_6/resources/views/listSinhvien.blade.php
@@ -1,1 +1,12 @@
-<?php
+
+@extends('layouts.app')
+@section('content')
+    <h2>Danh sach SV</h2>
+{{--    <p>{{ $page_description }}</p>--}}
+{{--    <h3>Danh sách công việc (Lấy từ Controller):</h3>--}}
+    <ul>
+        @foreach($danhsachSV as $sv)
+            <li>{{ $sv->ten_sinh_vien  }} - {{$sv -> email}}</li>
+        @endforeach
+    </ul>
+@endsection
